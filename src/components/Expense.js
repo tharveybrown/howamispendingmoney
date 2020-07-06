a62f30607d6c0268ad4468e4b00621d06259deff
tharveybrown <tharveybrown@gmail.com>
2020-07-06 18:23:31 -0400

    stuff

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -4,8 +4,9 @@ const Expense = ({ expense }) => {
   console.log(expense);
   return (
     <div>
-      <h4>{expense.amount}</h4>
-      <p>{expense.entity.name}</p>
+      <h4>$ {expense.amount}</h4>
+      <h5>{expense.entity.name}</h5>
+      <p>{expense.category}</p>
     </div>
   );
 };
