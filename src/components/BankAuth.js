96e6b81753c67c9a28f00633542894b0f9e4b07f
tharveybrown <tharveybrown@gmail.com>
2020-07-10 15:07:35 -0400

    env config

--- src/components/BankAuth.js
+++ src/components/BankAuth.js
@@ -5,13 +5,14 @@ import { PlaidLink } from "react-plaid-link";
 
 let env = runtimeEnv();
 const plaidPublicKey = env.REACT_APP_PLAID_PUBLIC_KEY;
+const plaidEnv = env.REACT_APP_PLAID_ENV;
 
 const BankAuth = (props) => {
   return (
     <div className="plaid-link">
       <PlaidLink
         clientName="Donation tracker"
-        env="sandbox"
+        env={plaidEnv}
         product={["auth", "transactions"]}
         publicKey={plaidPublicKey}
         onSuccess={props.onSuccess}
